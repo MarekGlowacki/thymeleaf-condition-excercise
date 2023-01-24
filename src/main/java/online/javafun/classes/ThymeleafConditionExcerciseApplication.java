ed053c5932005f9ac71d20752ddf431c3d3213fa
User <user@its.me>
2023-01-24 09:54:05 +0100

    Initial commit

---
+++ src/main/java/online/javafun/classes/ThymeleafConditionExcerciseApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafConditionExcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafConditionExcerciseApplication.class, args);
+    }
+
+}
